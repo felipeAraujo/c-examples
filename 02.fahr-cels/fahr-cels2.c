d7ddc340ae7527a972302b5936f3e9f792c6c5d0
* <felipe.at.1989@gmail.com>
2019-01-25 10:25:35 -0200

    fahr celsius examples

---
+++ 02.fahr-cels/fahr-cels2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+	int fahr, cels;
+	int steps = 20;
+	int lower = 0;
+	int upper = 300;
+
+	for(fahr = lower; fahr <= upper; fahr += steps) {
+		cels = 5 * (fahr - 32) / 9;
+		
+		printf("%3d\t%6d\n", fahr, cels);
+	}
+
+	return 0;
+}
