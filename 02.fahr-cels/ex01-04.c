43ffcced8445f56bac1354ce9eb9656889a8447f
* <felipe.at.1989@gmail.com>
2019-01-25 22:08:44 -0200

    exercises 1-3 & 1-4

---
+++ 02.fahr-cels/ex01-04.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main() {
+	float fahr, cels;
+	int steps = 20;
+	int lower = 0;
+	int upper = 300;
+	
+	printf("Celsius\t\tFahrenheint\n");
+	
+	for(cels = lower; cels <= upper; cels += steps) {
+		fahr = cels*9/5 +32;
+		
+		printf("%3.0f\t\t%6.1f\n", cels, fahr);
+	}
+
+	return 0;
+}
