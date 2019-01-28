79b2ee212830beb89745311caf365306c4baf096
* <felipe.at.1989@gmail.com>
2019-01-28 06:12:03 -0200

    examples until ex 1-10

---
+++ 03.cpi2o/cpi2o-integer.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+	int c;
+
+	c = getchar();
+	while (c != EOF) {
+		c = getchar();
+		printf("%d\n", c);
+	}
+
+	printf("\n");
+}
---
+++ 04.lncount/ex01-09.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main() {
+	int previous, actual;
+
+	previous = getchar();
+
+	putchar(previous);
+	while ((actual = getchar()) != EOF) {
+		if (previous == ' ' && actual == ' ') {
+			continue;
+		}
+		putchar(actual);
+		previous = actual;
+	}
+
+	printf("\n");
+
+	return 0;
+}
