83b2a6ec2802c81dc6828912ee229022fbbb43e5
Felipe Teixeira <felipe.at.1989@gmail.com>
2019-01-30 05:40:55 -0200

    EX01-16 resolved

---
+++ 07.functions/ex01-16.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+
+#define MAXLINE 1000
+
+int getline2(char line[], int lim);
+void copy2(char to[], char from[]);
+
+int main()
+{
+	int len;
+	int max;
+	int textlength;
+	char line[MAXLINE];
+	char longest[MAXLINE];
+
+	max = 0;
+	textlength = 0;
+	
+	while ((len = getline2(line, MAXLINE)) > 0) {
+		printf("len: %d\n", len);
+		textlength += len;
+		if (len > max) {
+			max = len;
+			copy2(longest, line);
+		}
+	}
+
+	if (max > 0)
+		printf("%s\n", longest);
+
+	printf("Text Length: %d\n", textlength);
+
+	return 0;
+}
+
+int getline2(char line[], int lim)
+{
+	int i, c;
+
+	for (i = 0; i < lim-1 && (c = getchar()) != EOF && c != '\n'; ++i)
+		line[i] = c;
+
+	if (c == '\n') {
+		line[i++] = c;
+	}
+
+	line[i] = '\0';
+	
+	return i;
+}
+
+void copy2(char to[], char from[])
+{
+	int i;
+
+	i = 0;
+	while ((to[i] = from[i]) != '\0')
+		++i;
+}
+
