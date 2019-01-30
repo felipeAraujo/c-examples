65fa6f6431ae568e0d40c2bb019633a6a5da6bf7
Felipe Teixeira <felipe.at.1989@gmail.com>
2019-01-29 22:43:37 -0200

    Full example copyline preventing warning message

--- 07.functions/cpyln.c
+++ 07.functions/cpyln.c
@@ -2,11 +2,51 @@
 
 #define MAXLINE 1000
 
-int getline(char line[], int maxline);
-void copy(char to[], char from[]);
+int getline2(char line[], int lim);
+void copy2(char to[], char from[]);
 
 int main()
 {
 	int len;
-	int max
+	int max;
+	char line[MAXLINE];
+	char longest[MAXLINE];
+
+	max = 0;
+	
+	while ((len = getline2(line, MAXLINE)) > 0)
+		if (len > max) {
+			max = len;
+			copy2(longest, line);
+		}
+
+	if (max > 0)
+		printf("%s", longest);
+
+	return 0;
 }
+
+int getline2(char line[], int lim) {
+	int c, i;
+
+	for (i = 0; i < lim-1 && (c = getchar()) != EOF && c != '\n'; ++i)
+		line[i] = c;
+
+	if (c == '\n') {
+		line[i++] = c;
+	}
+
+	line[i] = '\0';
+	
+	return i;
+}
+
+void copy2(char to[], char from[])
+{
+	int i;
+
+	i = 0;
+	while ((to[i] = from[i]) != '\0')
+		++i;
+}
+
