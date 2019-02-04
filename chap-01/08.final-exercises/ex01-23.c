dc4e7b848afba795f36bdd614948d9dfe5e73a96
Felipe Teixeira <felipe.at.1989@gmail.com>
2019-02-04 09:25:43 -0200

    [WIP] ex01-23

--- chap-01/08.final-exercises/ex01-23.c
+++ chap-01/08.final-exercises/ex01-23.c
@@ -1,18 +1,50 @@
 #include<stdio.h>
 
-#define OUTEVERYTHING 0
-#define INCOMMENT 1
-#define INCOMMENTCOMPLEX 2
-#define INSTRINGSIMPLE 3
-#define INSTRINGDOUBLE 4
+char checkIfComment(void);
+char handleInComment(void);
+char handleInCommentComplex(void);
+char handleInString(char);
 
 int main() {
     int c;
-    int stateC
 
     while ((c = getchar()) != EOF) {
+        char checkChar = '';
 
+        if (c == '/') {
+            checkChar = checkIfComment();
+
+            if (checkChar == EOF)
+                return 1;
+            else if (checkChar == '/')
+                checkChar = handleInComment();
+            else if (checkChar == '*')
+                checkChar = handleInCommentComplex();
+            else {
+                printf("/%c", checkChar);
+            }
+
+            if (checkChar == EOF)
+                return 1;
+
+            continue;
+        }
+
+        if (c == '"')
+            checkChar = handleInString('"');
+
+        if (c == '\'')
+            checkChar = handleInString('\'');
+
+        if (checkChar == EOF)
+            return 1;
     }
+
+    return 0;
+}
+
+char checkIfComment() {
+    return getchar();
 }
 
 char handleInComment() {
@@ -25,7 +57,6 @@ char handleInComment() {
 
 char handleInCommentComplex()
 {
-
     int stateFirstToFinish = 0;
     while((c = getchar()) != EOF) {
         if (c == '*')
@@ -41,10 +72,30 @@ char handleInCommentComplex()
     return EOF;
 }
 
-int handleInString(char commentString)
+char handleInString(char charString)
 {
+    int statePreviousBackslash = 0;
+
+    printf("%c", charString);
+
     while ((c = getchar()) != EOF)
     {
+        if (c == '\\' && !statePreviousBackslash) {
+            printf("%c", c);
+            statePreviousBackslash = 1;
+            continue;
+        }
+
+        if (c == charString && !statePreviousBackslash) {
+            printf("%c", c);
+            return '';
+        }
+
+        if (statePreviousBackslash) {
+            statePreviousBackslash = 0;
+        }
+
+        printf("%c", c);
     }
 
     return EOF;
