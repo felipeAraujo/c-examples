f2e22b13fc6bdb365e3e7801f9544268c6bd12fe
felipeAraujo <felipe.at.1989@gmail.com>
2019-02-08 10:39:24 -0200

    [WIP] ex02-01

---
+++ chap-02/01.ranges/ex02-01.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<limits.h>
+
+int main()
+{
+    printf("int limits: %d <=> %d\n", INT_MAX, INT_MIN);
+    printf("char limits: %d <=> %d\n", CHAR_MAX, CHAR_MIN);
+    printf("long limits: %d <=> %d\n", LONG_MAX, LONG_MIN);
+    printf("signed char limits: %d <=> %d\n", SCHAR_MAX, SCHAR_MIN);
+    printf("short limits: %d <=> %d\n", SHRT_MAX, SHRT_MIN);
+    printf("signed char limits: %d <=> %d\n", SCHAR_MAX, SCHAR_MIN);
+    printf("signed char limits: %d <=> %d\n", SCHAR_MAX, SCHAR_MIN);
+    printf("signed char limits: %d <=> %d\n", SCHAR_MAX, SCHAR_MIN);
+    printf("signed char limits: %d <=> %d\n", SCHAR_MAX, SCHAR_MIN);
+    printf("signed char limits: %d <=> %d\n", SCHAR_MAX, SCHAR_MIN);
+    printf("int limits: %d <=> %d\n", INT_MAX, INT_MIN);
+
+    return 0;
+}
+
