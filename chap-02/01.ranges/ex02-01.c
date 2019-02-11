5f603763407215da14387437306b179b4075be0f
felipeAraujo <felipe.at.1989@gmail.com>
2019-02-10 23:54:31 -0200

    ex02-01 complete to check on computer

--- chap-02/01.ranges/ex02-01.c
+++ chap-02/01.ranges/ex02-01.c
@@ -5,15 +5,15 @@ int main()
 {
     printf("int limits: %d <=> %d\n", INT_MAX, INT_MIN);
     printf("char limits: %d <=> %d\n", CHAR_MAX, CHAR_MIN);
-    printf("long limits: %d <=> %d\n", LONG_MAX, LONG_MIN);
+    printf("long limits: %ld <=> %ld\n", LONG_MAX, LONG_MIN);
     printf("signed char limits: %d <=> %d\n", SCHAR_MAX, SCHAR_MIN);
     printf("short limits: %d <=> %d\n", SHRT_MAX, SHRT_MIN);
     printf("signed char limits: %d <=> %d\n", SCHAR_MAX, SCHAR_MIN);
-    printf("signed char limits: %d <=> %d\n", SCHAR_MAX, SCHAR_MIN);
-    printf("signed char limits: %d <=> %d\n", SCHAR_MAX, SCHAR_MIN);
-    printf("signed char limits: %d <=> %d\n", SCHAR_MAX, SCHAR_MIN);
-    printf("signed char limits: %d <=> %d\n", SCHAR_MAX, SCHAR_MIN);
-    printf("int limits: %d <=> %d\n", INT_MAX, INT_MIN);
+    printf("unsigned char max: %d\n", UCHAR_MAX);
+    printf("unsigned int max: %u\n", UINT_MAX);
+    printf("unsigned long max %lu\n", ULONG_MAX);
+
+    printf("unsigned short max %d\n", USHRT_MAX);
 
     return 0;
 }
