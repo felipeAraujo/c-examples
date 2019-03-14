68376b30b1a33d08103190fcaf6ca72adb3431a3
felipeAraujo <felipe.at.1989@gmail.com>
2019-03-14 11:23:44 -0300

    ex3-03 [WIP]

--- chap-03/03.while-for/ex3-03.c
+++ chap-03/03.while-for/ex3-03.c
@@ -10,17 +10,33 @@ int main()
 
 void expand(char s1[], char s2[])
 {
-    char prev, current;
+    char range1, range2, prev, current;
     int slen = strlen(s1);
     int i;
     int j = 0;
 
+    range1 = '\0';
     prev = '\0';
-    for (i = 0; i < slen, ++i) {
-        current = s1[i];
-        if (prev != '\0' && prev != '-')
-            s2[j++] = prev;
+    range2 = '\0';
+    for (i = 0; i < slen - 1, ++i) {
+        if(s1[i+1] != '-' && s1[i] != '-')
+            s2[j++] = s1[i];
+        else if (s1[i+1] == '-' && s1[i] != '-')
+            range1 = s1[i];
+        else if (s1[i] == '-' && s1[i+1] != '-') {
+            range2 = s1[i+1];
+            if (range2 > range1) {
+                int temp = range1;
+                range1 = range2;
+                range2 = temp;
+            }
 
-        prev = current;
+            while (range1 <= range2)
+                s2[j++] = range1++;
+
+            range1 = '\0';
+            range2 = '\0';
+            ++i;
+        } 
     }
 }
